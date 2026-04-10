f7ee649d03b7f6ed4f642162aacc19ed9f8e1ea1
Giannis Grigoriadis <49716765+master3763@users.noreply.github.com>
2026-04-10 11:19:54 +0300

    Update transmitter.ino

--- src/transmitter/transmitter.ino
+++ src/transmitter/transmitter.ino
@@ -1,1 +1,63 @@
 
+#include <SPI.h>
+#include <LoRa.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is conntec to the Arduino digital pin 4
+#define ONE_WIRE_BUS 4
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+
+int counter = 0;
+
+void setup() {
+  Serial.begin(9600);
+
+   // Start up the library
+  sensors.begin();
+
+  while (!Serial);
+
+  Serial.println("LoRa Sender");
+
+  if (!LoRa.begin(433E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+
+  LoRa.setTxPower(10);
+  
+}
+
+void loop() {
+
+// Call sensors.requestTemperatures() to issue a global temperature and Requests to all devices on the bus
+  sensors.requestTemperatures(); //🌡🤒🔥
+  
+  Serial.print("Celsius temperature: ");
+  // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
+  Serial.print(sensors.getTempCByIndex(0)); 
+
+
+
+  Serial.print("Sending packet: ");
+  Serial.println(counter);
+
+  // send packet
+  LoRa.beginPacket();
+  LoRa.print("Celsius temperature from Lora: ");
+  LoRa.print(sensors.getTempCByIndex(0));
+
+  
+  
+  LoRa.endPacket();
+
+  counter++;
+
+  delay(5000);
+}
